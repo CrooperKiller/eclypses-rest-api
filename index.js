73abbea8b53d0c233cb468104a3716bf94a4558c
CrooperKiller <mishasprojects@gmail.com>
2022-07-19 09:57:01 -0600

    finished npm run start

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 3000;
 
 app.get(`/api/users/:id`, (req, res) => {
-  const person = users.filter((user, index) => {
+  const person = users.filter((user) => {
     return user.id === parseInt(req.params.id);
   });
   res.send(person[0]);
@@ -20,71 +20,71 @@ app.listen(PORT, () => {
 const users = [
   {
     id: 1,
-    first_name: "Blake",
-    last_name: "Wychard",
+    firstName: "Blake",
+    lastName: "Wychard",
     email: "bwychard0@fda.gov",
     gender: "Male",
   },
   {
     id: 2,
-    first_name: "Beck",
-    last_name: "Edinboro",
+    firstName: "Beck",
+    lastName: "Edinboro",
     email: "bedinboro1@is.gd",
     gender: "Male",
   },
   {
     id: 3,
-    first_name: "Denver",
-    last_name: "Jandel",
+    firstName: "Denver",
+    lastName: "Jandel",
     email: "djandel2@pagesperso-orange.fr",
     gender: "Male",
   },
   {
     id: 4,
-    first_name: "Ewen",
-    last_name: "Biaggetti",
+    firstName: "Ewen",
+    lastName: "Biaggetti",
     email: "ebiaggetti3@gmpg.org",
     gender: "Male",
   },
   {
     id: 5,
-    first_name: "Clementine",
-    last_name: "Ellsom",
+    firstName: "Clementine",
+    lastName: "Ellsom",
     email: "cellsom4@shinystat.com",
     gender: "Female",
   },
   {
     id: 6,
-    first_name: "Marnie",
-    last_name: "Barajas",
+    firstName: "Marnie",
+    lastName: "Barajas",
     email: "mbarajas5@wp.com",
     gender: "Genderfluid",
   },
   {
     id: 7,
-    first_name: "Britteny",
-    last_name: "Donnett",
+    firstName: "Britteny",
+    lastName: "Donnett",
     email: "bdonnett6@hao123.com",
     gender: "Female",
   },
   {
     id: 8,
-    first_name: "Lloyd",
-    last_name: "Farraway",
+    firstName: "Lloyd",
+    lastName: "Farraway",
     email: "lfarraway7@nbcnews.com",
     gender: "Male",
   },
   {
     id: 9,
-    first_name: "Derwin",
-    last_name: "Culshaw",
+    firstName: "Derwin",
+    lastName: "Culshaw",
     email: "dculshaw8@ovh.net",
     gender: "Male",
   },
   {
     id: 10,
-    first_name: "Pavla",
-    last_name: "Bedow",
+    firstName: "Pavla",
+    lastName: "Bedow",
     email: "pbedow9@google.com.hk",
     gender: "Female",
   },
